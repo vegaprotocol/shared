196fd70a88620536fa074e8801a52998e9166666
Valentin Trinque <val.trinque@gmail.com>
2022-03-30 12:14:58 +0200

    feat: Add crypto.AsSha256() helper

--- libs/crypto/hash.go
+++ libs/crypto/hash.go
@@ -2,7 +2,9 @@ package crypto
 
 import (
 	"crypto/rand"
+	"crypto/sha256"
 	"encoding/hex"
+	"fmt"
 	"strings"
 
 	"golang.org/x/crypto/sha3"
@@ -18,4 +20,11 @@ func RandomHash() string {
 	data := make([]byte, 10)
 	rand.Read(data)
 	return strings.ToUpper(hex.EncodeToString(Hash(data)))
+}
+
+func AsSha256(o interface{}) string {
+	h := sha256.New()
+	h.Write([]byte(fmt.Sprintf("%v", o)))
+
+	return fmt.Sprintf("%x", h.Sum(nil))
 }
